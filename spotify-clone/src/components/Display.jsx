4248b8d9d7d4cb3e9d9b271ab04938cd813b2696
juanbeniitez <tjuanbenite@gmail.com>
2024-08-21 18:16:28 +0200

    AlmbumItem and SongItem created and added to DisplayHome

--- spotify-clone/src/components/Display.jsx
+++ spotify-clone/src/components/Display.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {Routes,  Route } from 'react-router-dom'
-import DisplayHome from "./DisplayHome";
+import DisplayHome from "@components/DisplayHome";
 
 const Display = () => {
     return(
