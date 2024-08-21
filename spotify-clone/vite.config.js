2355a5c051b5c6e5ace7819d78652a5aac8073c2
juanbeniitez <tjuanbenite@gmail.com>
2024-08-21 16:17:46 +0200

    Created and styled Sidebar

--- spotify-clone/vite.config.js
+++ spotify-clone/vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Cómo simular dirname para crear alias para las rutas
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+    },
+  },
 })
